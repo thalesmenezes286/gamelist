29fdf49dee53143eb6c8b7ce85b4c00e12913b14
Thales R. Menezes <thalesmenezes286@gmail.com>
2024-11-01 09:55:54 -0300

    feat: initial commit

---
+++ gamelist-api/src/main/java/com/dev/gamelistapi/GamelistApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.gamelistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelistApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamelistApiApplication.class, args);
+    }
+
+}
---
+++ gamelist-api/src/test/java/com/dev/gamelistapi/GamelistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.gamelistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
